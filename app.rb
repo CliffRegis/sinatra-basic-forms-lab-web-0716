f18a1a957b28232e19c42345b1c0c214155f3a56
Clifton Regis <github email address>
2016-08-12 16:12:18 -0400

    puppy lab completed

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require_relative 'config/environment'
+require 'sinatra/base'
 
 class App < Sinatra::Base
+
+ get '/' do
+ 	
+ 	erb :index
+ end
+
+ get '/new' do
+ 	
+ 	erb :new
+ end
+ 
+ post '/display_puppy' do
+ 	@name = params[:name]
+ 	@breed = params[:breed]
+ 	@age = params[:age]
+
+ 	erb :display_puppy
+ end
 
 end
